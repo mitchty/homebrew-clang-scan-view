35fd789742a056e458997fcd13aefb1fcd04f9f2
Mitch Tishmack <mitch.tishmack@gmail.com>
2014-06-18 17:29:01 -0500

    Initial commit.

---
+++ Formula/clang-scan-view.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+#-*-mode: Ruby; coding: utf-8;-*-
+require 'formula'
+
+class ClangScanView < Formula
+  homepage 'http://llvm.org'
+  url "http://llvm.org/svn/llvm-project/cfe/trunk/tools/scan-view", :using => :svn
+  depends_on 'svn'
+  depends_on 'llvm' => 'with-clang'
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/'scan-view'
+  end
+end
---
+++ readme.md
@@ -0,0 +1,9 @@
+homebrew-clang-scan-view
+==============
+
+Homebrew tap for the clang static analyzer scan-view tool.
+
+# Installation
+
+    brew tap mitchty/clang-scan-view
+    brew install clang-scan-view
